aa7080fb2d9e7ff74e11e2e4c3cc6536656e9460
Boris Barbulovski <bbarbulovski@gmail.com>
2021-05-24 15:05:50 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,42 @@
+### Features
+Recover photos from sdcard(or sdcard image file) taken with Nikon DSLR(Tested on D3200).
+
+### How to build
+`meson setup builddir`
+`cd builddir`
+`ninja`
+
+### Run
+```shell
+$ ./nikon_jpeg_recovery ../sdcard.img .
+Found jpeg at address 0x820000 - Date image created: 2015:11:21 12:24:55
+Found jpeg at address 0xf08000 - Date image created: 2015:11:21 12:40:36
+Found jpeg at address 0x1580000 - Date image created: 2015:11:21 12:41:01
+Found jpeg at address 0x1bc0000 - Date image created: 2015:11:21 12:41:13
+Found jpeg at address 0x2218000 - Date image created: 2015:11:21 12:42:28
+Found jpeg at address 0x2888000 - Date image created: 2015:11:21 12:45:33
+Found jpeg at address 0x2ed0000 - Date image created: 2015:11:21 12:46:38
+Found jpeg at address 0x3510000 - Date image created: 2015:11:21 12:46:47
+Found jpeg at address 0x3bd8000 - Date image created: 2015:11:21 12:46:56
+Found jpeg at address 0x42f0000 - Date image created: 2015:11:21 12:47:07
+Found jpeg at address 0x4938000 - Date image created: 2015:11:21 12:49:06
+Found jpeg at address 0x4f88000 - Date image created: 2015:11:21 12:50:07
+Found jpeg at address 0x56b8000 - Date image created: 2015:11:21 12:50:20
+Found jpeg at address 0x5cf8000 - Date image created: 2015:11:21 12:50:27
+Found jpeg at address 0x6368000 - Date image created: 2015:11:21 12:54:45
+Found jpeg at address 0x69f8000 - Date image created: 2015:11:21 12:54:53
+Found jpeg at address 0x7078000 - Date image created: 2015:11:21 12:55:00
+Found jpeg at address 0x7770000 - Date image created: 2015:11:21 12:55:39
+Found jpeg at address 0x7e48000 - Date image created: 2015:11:21 12:55:41
+Found jpeg at address 0x84e0000 - Date image created: 2015:11:21 12:56:33
+Found jpeg at address 0x8bb0000 - Date image created: 2015:11:21 12:56:46
+Found jpeg at address 0x92e0000 - Date image created: 2015:11:21 12:56:49
+Found jpeg at address 0x9a18000 - Date image created: 2015:11:21 12:56:52
+Found jpeg at address 0xa158000 - Date image created: 2015:11:21 12:56:58
+Found jpeg at address 0xa7c0000 - Date image created: 2015:11:21 12:57:00
+Found jpeg at address 0xaee8000 - Date image created: 2015:11:21 12:57:49
+Found jpeg at address 0xb5c8000 - Date image created: 2015:11:21 12:58:49
+Found jpeg at address 0xbc70000 - Date image created: 2015:11:21 12:58:55
+Found jpeg at address 0xc2f8000 - Date image created: 2015:11:21 12:59:00
+...
+```
---
+++ main.c
@@ -0,0 +1,140 @@
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+
+#define JPEG_HEADER 0xd8ff
+#define JPEG_END_TAG 0xd9ff
+#define JPEG_MIN_SIZE (1 * 1024 * 1024)
+#define JPEG_MAX_SIZE (64 * 1024 * 1024)
+
+// TODO: Implement proper way to get jpeg size.
+static uint64_t get_jpeg_size(const char *img)
+{
+    uint64_t pos = 0;
+
+    for(pos = JPEG_MIN_SIZE; pos < JPEG_MAX_SIZE; pos++)
+    {
+        uint16_t end_tag = 0;
+
+        end_tag = *((uint16_t *)(img + pos));
+        if (end_tag == JPEG_END_TAG)
+        {
+            return pos + 2;
+        }
+    }
+
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    struct stat img_stat;
+    char *img_file = NULL;
+    void *img_data = NULL;
+    uint64_t src_size = 0;
+    int img_fd = 0;
+
+    if (argc < 3)
+    {
+        fprintf(stderr, "Usage nikon-jpeg-recovery <img_file> <dest_dir>\n");
+        return EXIT_FAILURE;
+    }
+
+    setbuf(stdout, NULL); // disable buffering on stdout
+
+    img_file = argv[1];
+
+    // mmap file.
+    img_fd = open(argv[1], O_RDONLY);
+    if (img_fd == -1)
+    {
+        fprintf(stderr, "Error opening image file(%s). Error: %s\n", img_file, strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    if (fstat(img_fd, &img_stat) != 0)
+    {
+        fprintf(stderr, "Error stating image file(%s). Error: %s\n", img_file, strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    img_data = mmap(NULL, img_stat.st_size, PROT_READ, MAP_PRIVATE, img_fd, 0);
+    if (img_data == MAP_FAILED)
+    {
+        fprintf(stderr, "Error maping image file(%s). Error: %s\n", img_file, strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    src_size = img_stat.st_size;
+
+    // search for jpeg header on every sector start(512b)
+    for(uint64_t c = 0; c < (src_size - 512); c += 512)
+    {
+        void *ptr = NULL;
+        uint16_t hdr = 0;
+
+        ptr = (char *)img_data + c;
+        hdr = *((uint16_t *)ptr);
+
+        if (hdr == JPEG_HEADER)
+        {
+            const char *producer = (char *)ptr + 0xa0;
+
+            if (strcmp(producer, "NIKON CORPORATION") == 0)
+            {
+                const char *date_str = NULL;
+                uint64_t img_size = 0;
+
+                date_str = (char *)ptr + 0xdc;
+
+                printf("Found jpeg at address 0x%"PRIx64" - Date image created: %s\n", c, date_str);
+
+                img_size = get_jpeg_size(ptr);
+
+                if (img_size > 0)
+                {
+                    ssize_t written = 0;
+                    int out_fd = 0;
+
+                    int str_size = snprintf(NULL, 0, "%s/IMG_%s.jpg", argv[2], date_str);
+                    char dest_img_name[str_size + 1];
+
+                    snprintf(dest_img_name, sizeof dest_img_name, "%s/IMG_%s.jpg", argv[2], date_str);
+
+                    out_fd = open(dest_img_name, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+                    if (out_fd == -1)
+                    {
+                        fprintf(stderr, "Error creating img file(%s). Error: %s\n", dest_img_name, strerror(errno));
+                        break;
+                    }
+
+                    written = write(out_fd, ptr, img_size);
+                    if (written < 0)
+                    {
+                        fprintf(stderr, "Error writing img file(%s). Error: %s\n", dest_img_name, strerror(errno));
+                    }
+                    else if (written != img_size)
+                    {
+                        fprintf(stderr, "Partial write to img file(%s). Error: %s\n", dest_img_name, strerror(errno));
+                    }
+
+                    close(out_fd);
+                }
+            }
+        }
+    }
+
+    munmap(img_data, src_size);
+
+    close(img_fd);
+
+    return EXIT_SUCCESS;
+}
